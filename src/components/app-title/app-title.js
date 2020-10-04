348c01d1400f372560b3a99b4bd5bc1477d67840
Alexander <test.test.com>
2020-10-04 15:29:42 +0300

    change title

--- src/components/app-title/app-title.js
+++ src/components/app-title/app-title.js
@@ -4,7 +4,7 @@ import React from 'react';
 import './app-title.css';
 
 const AppTitle = () => {
-    const title = 'ToDo App';
+    const title = 'This is ToDo App';
     return <h1 className="appTitle">{ title }</h1>;
   }
   
