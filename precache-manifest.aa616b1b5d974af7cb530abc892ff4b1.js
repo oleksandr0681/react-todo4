77e48b56cee5f4611d1403e373744d959cc275d4
Alexander <test.test.com>
2020-10-04 14:18:26 +0300

    Updates

---
+++ precache-manifest.aa616b1b5d974af7cb530abc892ff4b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4e5ca2dd68db93ab7e4fc8d09b0ca7",
+    "url": "/react-todo4/index.html"
+  },
+  {
+    "revision": "e246c75a6447efff2a4b",
+    "url": "/react-todo4/static/css/main.6786a9bd.chunk.css"
+  },
+  {
+    "revision": "ce558c1d005a7ac643e3",
+    "url": "/react-todo4/static/js/2.3fde2acb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-todo4/static/js/2.3fde2acb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e246c75a6447efff2a4b",
+    "url": "/react-todo4/static/js/main.d8a0b028.chunk.js"
+  },
+  {
+    "revision": "b387f796a5929b97b333",
+    "url": "/react-todo4/static/js/runtime-main.087666ad.js"
+  },
+  {
+    "revision": "df70b00589269cc7e903d0e3ba9db627",
+    "url": "/react-todo4/static/media/react.df70b005.png"
+  }
+]);
